248c3fc30760e214d248daf57bccabf9f48499e3
mxordn <m.heffter@me.com>
2024-12-15 21:07:38 +0100

    github version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-export const HOST: string = 'https://klavier-app-api.satzlehre-online.de'
-// 'http://localhost:8000'
-export const Frontend_HOST: string = 'https://piano-lab.satzlehre-online.de/trainer'
-// http://localhost:4200/trainer
-//'http://localhost:4200'
+export const HOST: string = 'http://localhost:8000'
+
+export const Frontend_HOST: string = 'http://localhost:4200/trainer'
 
 @Component({
   selector: 'app-root',
